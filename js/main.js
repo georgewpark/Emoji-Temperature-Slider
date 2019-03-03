6b3ee71f9a3fef417ee6de6e1f1d916c055af30a
George Park <georgewpark@outlook.com>
2019-03-03 00:41:08 +0000

    add animation for codepen preview window

--- js/main.js
+++ js/main.js
@@ -1,27 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const emoji = document.querySelector('.emoji'),
-          slider = document.querySelector('.slider'),
-          tempOutput = document.querySelector('.temperature-output');
+        slider = document.querySelector('.slider'),
+        tempOutput = document.querySelector('.temperature-output'),
+        displayTemp = temperature => {
+            //Display temperature
+            tempOutput.textContent = temperature;
 
-    slider.addEventListener('input', () => {
-
-        const temperature = slider.value;
-
-        //Display temperature
-        tempOutput.textContent = temperature;
-
-        //Display emoji
-        if (temperature >= 0 && temperature <= 8) {
-            emoji.textContent = '🥶';
-        } else if (temperature > 8 && temperature <= 16) {
-            emoji.textContent = '😬';
-        } else if (temperature > 16 && temperature <= 24) {
-            emoji.textContent = '😊';
-        } else if (temperature > 24 && temperature <= 32) {
-            emoji.textContent = '😅';
-        } else {
-            emoji.textContent = '🥵';
+            //Display emoji
+            if (temperature >= 0 && temperature <= 8) {
+                emoji.textContent = '🥶';
+            } else if (temperature > 8 && temperature <= 16) {
+                emoji.textContent = '😬';
+            } else if (temperature > 16 && temperature <= 24) {
+                emoji.textContent = '😊';
+            } else if (temperature > 24 && temperature <= 32) {
+                emoji.textContent = '😅';
+            } else {
+                emoji.textContent = '🥵';
+            }
         }
-    });
+
+    slider.addEventListener('input', () => displayTemp(slider.value));
+
+    //CodePen preview window
+    if (location.pathname.includes('fullcpgrid')) {
+
+        let temperature = 0;
+
+        const interval = setInterval(() => {
+
+            //Remove interval if max temperature is reached
+            if (temperature === 40) clearInterval(interval);
+
+            //Update slider value
+            slider.value = temperature;
+
+            //Display temperature and emoji
+            displayTemp(temperature);
+
+            //Increase temperature
+            temperature++;
+
+        }, 95);
+    }
 });
